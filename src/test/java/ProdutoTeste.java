f998f7282c08ca562e99e58ba0769925664057c2
afonsooe <afonsoerivelton@gmail.com>
2026-04-23 17:13:00 -0300

    Adiciona teste de criação válida para Produto

---
+++ src/test/java/ProdutoTeste.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProdutoTeste {
+
+    @Test
+    void deveCriarProdutoComNomeEPrecoValidos() {
+        Produto produto = new Produto("Café Expresso", 5.00);
+
+        assertEquals("Café Expresso", produto.getNome());
+        assertEquals(5.00, produto.getPreco());
+    }
+}
