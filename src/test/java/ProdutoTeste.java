f818c13fb1e58e3cf70a02273f8c6ab0e3e57cb7
Felipe Oliveira <felipemendes0507@gmail.com>
2026-04-23 21:22:53 -0300

    Ajustes

--- src/test/java/ProdutoTeste.java
+++ src/test/java/ProdutoTeste.java
@@ -1,7 +1,6 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
 
 public class ProdutoTeste {
 
