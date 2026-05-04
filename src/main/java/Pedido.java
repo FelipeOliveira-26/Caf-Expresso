1c1f8cbdd02d988948251543758e1d4b92a0f636
afonsooe <afonsoerivelton@gmail.com>
2026-05-04 19:19:06 -0300

    Adiciona classe Pedido com lista de itens

---
+++ src/main/java/Pedido.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    private List<ItemPedido> itens;
+
+    public Pedido() {
+        this.itens = new ArrayList<>();
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+}
